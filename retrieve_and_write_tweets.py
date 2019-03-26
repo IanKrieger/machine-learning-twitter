189e4fcd75bb48e015885e632274bc32aebc2f3f
Ian Krieger <iankrieger@ians-mbp.attlocal.net>
2019-03-25 21:06:56 -0400

    temp commit for debug

--- retrieve_and_write_tweets.py
+++ retrieve_and_write_tweets.py
@@ -1,7 +1,7 @@
 import tweepy
 
-api_key = "api_key"
-api_secret = "api_secret"
+consumer_key = "api_key"
+consumer_secret = "api_secret"
 access_key = "access_key"
 access_secret = "access_secret"
 
@@ -9,20 +9,18 @@ def get_tweets(username, count):
 
 	#http://tweepy.readthedocs.org/en/v3.1.0/getting_started.html#api
 	#Tweepy is a little easier then twitter api docs :)
-	auth = tweepy.OAuthHandler(api_key, api_secret)
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_key, access_secret)
 	api = tweepy.API(auth)
 
-	tweet_count = count
-
 	#get tweets & write to file as you go (memory consideration)
-	outfile = username + "_tweets.txt"
-	file = open(outfile, "w+")
+	#outfile = username + "_tweets.txt"
+	#file = open(outfile, "w+")
 
-	for tweet in tweepy.Cursor(api.user_timeline, screen_name = username).items(tweet_count):
-		file.write(tweet.text.encode("utf-8") + "\n")
+	for tweet in tweepy.Cursor(api.home_timeline, id=username).items(count):
+		print(tweet.text)
 
-	file.close()
+	#file.close()
 
 
 user = input("What user do you want tweets for?: ")
